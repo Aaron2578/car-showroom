fbb1f126c7bdb2b2ea4f4687a3338efd5babc5d8
Aaron <115331438+Aaron2578@users.noreply.github.com>
2025-05-07 15:35:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -83,6 +83,23 @@ const carsData = [
     desp: "Racing ,Fastest & Luxurious Vehicles for rich persons",
   },
 ];
+const Services = [
+  { id: 1, service: "Repair", price: 2000, photo: "./repair.jpg" },
+  { id: 2, service: "Water Wash", price: 1000, photo: "./water wash.jpg" },
+  { id: 3, service: "Engine", price: 10000, photo: "./engine.jpg" },
+  { id: 4, service: "Painting", price: 5000, photo: "./paint.jpg" },
+  { id: 5, service: "Modification", price: 12000, photo: "./modification.jpg" },
+  { id: 1, service: "Repair", price: 2000, photo: "./repair.jpg" },
+  { id: 2, service: "Water Wash", price: 1000, photo: "./water wash.jpg" },
+  { id: 3, service: "Engine", price: 10000, photo: "./engine.jpg" },
+  { id: 4, service: "Painting", price: 5000, photo: "./paint.jpg" },
+  { id: 5, service: "Modification", price: 12000, photo: "./modification.jpg" },
+  { id: 1, service: "Repair", price: 2000, photo: "./repair.jpg" },
+  { id: 2, service: "Water Wash", price: 1000, photo: "./water wash.jpg" },
+  { id: 3, service: "Engine", price: 10000, photo: "./engine.jpg" },
+  { id: 4, service: "Painting", price: 5000, photo: "./paint.jpg" },
+  { id: 5, service: "Modification", price: 12000, photo: "./modification.jpg" },
+];
 export default function App() {
   return (
     <div>
@@ -99,29 +116,70 @@ function Header() {
       <div>
         <div className="header">
           <img src="./bolero.png" alt="indica car" height={"50px"} />
-          <div>
+          <div className="heading">
             <h1>Sakthivel Motors</h1>
             <p>Sales & Services</p>
           </div>
         </div>
-        
       </div>
     </header>
   );
 }
 function Body() {
-  const [isOpen, setIsOpen] = useState(false);
-
+  // const [isOpen, setIsOpen] = useState(true);
+  const [home, setHome] = useState(true);
+  const [tab, setTab] = useState(false);
+  const [tabs, setTabs] = useState(false);
   return (
     <div className="menu">
-      <button onClick={() => setIsOpen(!isOpen)}>Sales</button>
-      {isOpen === true && (
+      {/* <button onClick={() => setIsOpen(!isOpen)}>Sales</button> */}
+      <div className="btn-wrapper">
+        <button onClick={() => setHome(!home)}>Home</button>
+        <button onClick={() => setTab(!tab)}>Sales</button>
+        <button onClick={() => setTabs(!tabs)}>Services</button>
+      </div>
+      {home === true ? <Home /> : ""}
+      {tab === true ? (
         <ul className="cars">
           {carsData.map((cars) => (
             <CarItems carObj={cars} />
           ))}
         </ul>
+      ) : (
+        ""
       )}
+      {tabs === true ? (
+        <ul className="cars">
+          {Services.map((ser) => (
+            <CarServices serObj={ser} />
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+function Home() {
+  return (
+    <div className="home">
+      <img src="./jeep.png" alt="jeep" />
+      <div>
+        <h2 style={{ margin: "0px 0px 20px 0px", color: "red" }}>Jeep 4X4</h2>
+        <p>
+          Jeep is an American automobile manufacturer. The company was founded
+          in 1943 and was acquired by Chrysler in 1987. Jeep is now owned by
+          Stellantis, a European-American conglomerate. Jeep was also the name
+          of a light vehicle of World War II developed by the U.S. Army
+          Quartermaster Corps. The jeep weighed 1 1/4 tons, was powered by a
+          four-cylinder engine, and was classed as a quarter-ton truck in
+          carrying capacity. A four-wheel drive vehicle of one-half- to
+          one-and-one-half-ton [0.45 to 1.36 tonnes] capacity for reconnaissance
+          or other army duty. A term applied to the bantam-cars, and
+          occasionally to other motor vehicles (U.S.A.) in the Air Corps, the
+          Link Trainer; in the armored forces, the 1⁄2-ton command vehicle.
+        </p>
+      </div>
     </div>
   );
 }
@@ -148,6 +206,31 @@ For More details`
       >
         More
       </button>
+    </li>
+  );
+}
+function CarServices({ serObj }) {
+  return (
+    <li className="car">
+      {/* <h3 style={{ margin: "20px" }}>{carObj.brand}</h3> */}
+      <img src={serObj.photo} alt={serObj.name} height={"80px"} />
+      <h4 style={{ margin: "10px" }}>{serObj.service}</h4>
+      <p style={{ margin: "10px" }}>
+        <b>
+          <span style={{ color: "red" }}>Price :</span> ₹{serObj.price}
+        </b>
+      </p>
+      {/* <button
+        onClick={() =>
+          alert(
+            `I am an ${serObj.name} car from ${serObj.brand} Automobiles. 
+I am in development. 
+For More details`
+          )
+        }
+      >
+        More
+      </button> */}
     </li>
   );
 }
